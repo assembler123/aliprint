6c95a87b2b8020868f7bfb4a956aa2069bd8b923
unknown <qasima9@gmail.com>
2021-07-09 00:01:01 +0530

    fff

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const path=require('path')
+app.use(express.static(path.join(__dirname)))
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+app.listen(80,()=>{
+    console.log("Server is listening on 80");
+})
